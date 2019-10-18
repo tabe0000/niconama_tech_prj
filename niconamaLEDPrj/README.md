ef5ca17bc7c03921583a08e43c98e25552f3b647
tabe <tabetabe.unity@gmail.com>
2019-10-19 02:36:11 +0900

    add readme and add licenses

---
+++ niconamaLEDPrj/README.md
@@ -0,0 +1,9 @@
+# niconamaLEDPrj
+
+## OverView
+ニコニコ生放送で打たれた、特定のコメントによってArduinoのLEDを制御できる。  
+コメントLチカ的なの。
+
+## Description
+Node jsでサーバーを立てて、そこにArduinoに対する処理を書いてある。  
+今回はLチカをしたが、今後ははちゅねミクにコメントでネギを振らせたい。
--- niconamaLEDPrj/nicoclient.js
+++ niconamaLEDPrj/nicoclient.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
+
+// This software includes the work that is distributed in the Apache License 2.0.
+// このソフトウェアは、 Apache 2.0ライセンスで配布されている製作物が含まれています。
+
+//Copyright (c) 2012, 2013, 2014 Rick Waldron waldron.rick@gmail.com Licensed under the MIT license. 
+//Copyright(c) 2014, 2015 The Johnny - Five Contributors Licensed under the MIT license.
+
+
 var WebSocketClient = require('websocket').client;
 const fs = require('fs');
 
